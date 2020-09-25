799981f70b29f9b1415126063c0ea00812760f88
hongyue-qiu <994059641@qq.com>
2020-09-25 15:12:17 +0800

    finished the order index

--- front-end/src/Order.js
+++ front-end/src/Order.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import './order.css';
 
 class Order extends Component {
     render() {
         return (<div className="order">
             <div className="main">
-                订单信息
+                <table className="order_table">
+                    <tr>
+                        <td className="">名字</td>
+                        <td className="">单价</td>
+                        <td className="">数量</td>
+                        <td className="">单位</td>
+                        <td className="">操作</td>
+                    </tr>
+                    <tr>
+                        <td className="">可乐</td>
+                        <td className="">1</td>
+                        <td className="">2</td>
+                        <td className="">瓶</td>
+                        <td className="">
+                            <input type="button" className="delete" value="删除"/>
+                        </td>
+                    </tr>
+                </table>
             </div>
         </div>);
     }
